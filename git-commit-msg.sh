247a71581cf7fb1883b73f9dbe982c990ac53625
Razeen <razeen.cheng@gmail.com>
2020-04-14 10:41:21 +0800

    chore: golang pre-commit go vet unique go dirs

--- git-commit-msg.sh
+++ git-commit-msg.sh
@@ -3,11 +3,11 @@
 COMMIT_MSG=`cat $1 | egrep "^(feat|fix|docs|chore)(\(\w+\))?:\s(\S|\w)+"`
 
 if [ "$COMMIT_MSG" = "" ]; then
-	echo "Commit Message 不规范，请检查!\n"
+	echo "Commit Message Irregular，Please check!\n"
 	exit 1
 fi
 
 if [ ${#COMMIT_MSG} -lt 15 ]; then
-	echo "Commit Message 太短了，请再详细点!\n"
+	echo "Commit Message Too Short，Please show me more detail!\n"
 	exit 1
 fi
--- golang-pre-commit.sh
+++ golang-pre-commit.sh
@@ -10,12 +10,27 @@ allgofiles=$(git diff --cached --name-only --diff-filter=ACM | grep '.go$')
 
 # 过滤vendor的
 gofiles=()
+godirs=()
 for allfile in ${allgofiles[@]}; do 
     if [[ $allfile == "vendor"* ]];then
         continue
     else
-		gofiles+=("$allfile")
-	fi
+        gofiles+=("$allfile")
+
+        # 文件夹去重
+        existdir=0
+        dir=`echo "$allfile" |xargs -n1 dirname|sort -u`
+        for somedir in ${godirs[@]}; do
+            if [[ $dir == $somedir ]]; then 
+                existdir=1
+                break
+            fi
+        done
+
+        if [[ $existdir -eq 0 ]]; then 
+            godirs+=("$dir")
+        fi
+    fi
 done
 
 [ -z "$gofiles" ] && exit 0
@@ -68,18 +83,18 @@ fi
 
 # go vet 静态错误检查
 show_vet_header=true
-for file in ${gofiles[@]} ; do
-	dir=`echo "$file" |xargs -n1 dirname|sort -u`
-	vet=$(go vet $PWD/$dir 2>&1)
-	if [ -n "$vet" -a $show_vet_header = true ] ; then
-		echo "govet:"
-		show_vet_header=false
-	fi
-	if [ -n "$vet" ] ; then
-		echo "$vet\n"
-		has_errors=1
-	fi
+for dir in ${godirs[@]} ; do
+    vet=$(go vet $PWD/$dir 2>&1)
+    if [ -n "$vet" -a $show_vet_header = true ] ; then
+	echo "govet FAIL:"
+	show_vet_header=false
+    fi
+    if [ -n "$vet" ] ; then
+	echo "$vet\n"
+	has_errors=1
+    fi
 done
 
 
 exit $has_errors
+
